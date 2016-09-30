dd053d7255cd328af3175fcce77c07f1d0971062
jcnunes <joana.nunes228@gmail.com>
2016-09-30 23:03:09 +0200

    Added more contacts

--- data/contacts.js
+++ data/contacts.js
@@ -17,4 +17,110 @@ module.exports = [{
     last_name: 'B. Jones',
     email: 'BarbaraBJones@dayrep.com',
     main_number: '312-537-3355'
-}];
+}, {
+    contact_id: 'PatrickSBuck@dayrep.com',
+    first_name: 'Patrick',
+    last_name: 'S. Buck',
+    email: 'PatrickSBuck@dayrep.com',
+    main_number: '954-802-6731'
+},  {
+    contact_id: 'DonaldLPeay@dayrep.com',
+    first_name: 'Donald',
+    last_name: 'L. Peay',
+    email: 'DonaldLPeay@dayrep.com',
+    main_number: '601-722-7810'
+},  {
+    contact_id: 'CarlaEDouglas@jourrapide.com',
+    first_name: 'Carla',
+    last_name: 'E. Douglas',
+    email: 'CarlaEDouglas@jourrapide.com',
+    main_number: '508-297-8522'
+}, {
+    contact_id: 'ShawnCMueller@teleworm.us',
+    first_name: 'Shawn',
+    last_name: 'C. Mueller',
+    email: 'ShawnCMueller@teleworm.us',
+    main_number: '419-424-2494'
+}, {
+    contact_id: 'RichardVPew@armyspy.com',
+    first_name: 'Richard',
+    last_name: 'V. Pew',
+    email: 'RichardVPew@armyspy.com',
+    main_number: '337-290-9387'
+}, {
+    contact_id: 'EdnaDWebb@jourrapide.com',
+    first_name: 'Edna',
+    last_name: 'D. Webb',
+    email: 'EdnaDWebb@jourrapide.com',
+    main_number: '318-521-9482'
+}, {
+    contact_id: 'LouiseJJohnston@jourrapide.com',
+    first_name: 'Louise',
+    last_name: 'J. Johnston',
+    email: 'LouiseJJohnston@jourrapide.com',
+    main_number: '352-625-3762'
+}, {
+    contact_id: 'RachelleJMiller@armyspy.com',
+    first_name: 'Rachelle',
+    last_name: 'J. Miller',
+    email: 'RachelleJMiller@armyspy.com',
+    main_number: '631-880-3223'
+}, {
+    contact_id: 'PennyJHopkins@teleworm.us',
+    first_name: 'Penny',
+    last_name: 'J. Hopkins',
+    email: 'PennyJHopkins@teleworm.us',
+    main_number: '617-266-9772'
+}, {
+    contact_id: 'AnneLJones@armyspy.com',
+    first_name: 'Anne',
+    last_name: 'L. Jones',
+    email: 'AnneLJones@armyspy.com',
+    main_number: '949-717-5928'
+}, {
+    contact_id: 'JoyceWSyverson@armyspy.com',
+    first_name: 'Joyce',
+    last_name: 'W. Syverson',
+    email: 'JoyceWSyverson@armyspy.com',
+    main_number: '479-846-7951'
+}, {
+    contact_id: 'MelissaASmith@armyspy.com',
+    first_name: 'Melissa',
+    last_name: 'A. Smith',
+    email: 'MelissaASmith@armyspy.com',
+    main_number: '443-513-2584'
+}, {
+    contact_id: 'EddieSGlass@armyspy.com',
+    first_name: 'Eddie',
+    last_name: 'S. Glass',
+    email: 'EddieSGlass@armyspy.com',
+    main_number: '219-755-1682'
+},  {
+    contact_id: 'JulianNFunk@rhyta.com',
+    first_name: 'Julian',
+    last_name: 'N. Funk',
+    email: 'EddieSGlass@armyspy.com',
+    main_number: '219-755-1682'
+}, {
+    contact_id: 'EricKBrookshire@dayrep.com',
+    first_name: 'Eric',
+    last_name: 'K. Brookshire',
+    email: 'EricKBrookshire@dayrep.com',
+    main_number: '509-336-5069'
+}, {
+    contact_id: 'VickieZRuth@teleworm.us',
+    first_name: 'Vicki',
+    last_name: 'Z. Ruth',
+    email: 'VickieZRuth@teleworm.us',
+    main_number: '254-220-8241'
+},
+
+
+
+
+
+
+
+
+
+];
--- data/extendedContacts.js
+++ data/extendedContacts.js
@@ -29,4 +29,235 @@ module.exports = [{
         label: 'Occupation',
         value: 'Welding worker'
     }]
-}];
+}, {
+    contact_id: 'PatrickSBuck@dayrep.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'January 10, 1975'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'travelhookah.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Athletic trainer'
+    }]
+}, {
+    contact_id: 'DonaldLPeay@dayrep.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'September 19, 1967'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'tcgreens.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Health information technician'
+    }]
+}, {
+    contact_id: 'CarlaEDouglas@jourrapide.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'October 20, 1996'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'jellowman.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Podiatric surgeon'
+    }]
+}, {
+    contact_id: 'ShawnCMueller@teleworm.us',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'February 28, 1984'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'dawparts.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Food service worker'
+    }]
+},  {
+    contact_id: 'RichardVPew@armyspy.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'October 29, 1969'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'horsezens.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Vocational education teacher'
+    }]
+},  {
+    contact_id: 'EdnaDWebb@jourrapide.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'January 17, 1951'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'crownedketylo.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Employment counselor'
+    }]
+}, {
+    contact_id: 'LouiseJJohnston@jourrapide.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'July 21, 1944'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'geospentech.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Guitar repairer'
+    }]
+}, {
+    contact_id: 'PennyJHopkins@teleworm.us',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'November 12, 1966'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'ronswansonisms.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Psychiatrist'
+    }]
+},  {
+    contact_id: 'AnneLJones@armyspy.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'December 24, 1976'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'paradorlamorena.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Paymaster'
+    }]
+}, {
+    contact_id: 'JoyceWSyverson@armyspy.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'July 23, 1941'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'kirtonkayak.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Support service manager'
+    }]
+}, {
+    contact_id: 'MelissaASmith@armyspy.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'July 25, 1962'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'myhottubfilters.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Credit clerk'
+    }]
+}, {
+    contact_id: 'EddieSGlass@armyspy.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'September 25, 1931'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'fewpixelsmedia.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Corporate office services manager'
+    }]
+}, {
+    contact_id: 'JulianNFunk@rhyta.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'February 1, 1938'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'vigistan.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Dishwasher'
+    }]
+}, {
+    contact_id: 'EricKBrookshire@dayrep.com',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'June 13, 1947'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'hmchangtai.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Wealth manager'
+    }]
+}, {
+    contact_id: 'VickieZRuth@teleworm.us',
+    details: [{
+        key: 'birthday',
+        label: 'Birthday',
+        value: 'March 23, 1936'
+    }, {
+        key: 'website',
+        label: 'Website',
+        value: 'spyntel.com'
+    }, {
+        key: 'occupation',
+        label: 'Occupation',
+        value: 'Gaming surveillance officer'
+    }]
+},
+
+
+
+
+
+];
